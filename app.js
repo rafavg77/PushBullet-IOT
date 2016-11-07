73d3b9039f43f40c25e349c84127b70bf02dd619
Tota77 <rafavg77@gmail.com>
2016-11-06 21:15:45 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var PushBullet = require('pushbullet');
-var pusher = new PushBullet('API-KEY');
+var pusher = new PushBullet('API_KEY');
 var request = require("request");
 var stream = pusher.stream();
 var device = 'Domotic Pi';
 var url = 'http://192.168.0.250:3000/api/red/';
+var exec = require('child_process').exec, child;
 
 
 function notify(pusher,device,message){
@@ -30,8 +31,19 @@ function dater(){
   return date;
 };
 
-function logger(message2log){
-  console.log(dater() + ' - ' + message2log);
+function logger(message2log,online){
+  if(online){
+    child = exec('logger_api " '+ message2log +' " > /dev/null 2>&1',
+    function (error, stdout, stderr) {
+      console.log(dater() + " stdout: " + stdout);
+      console.log(dater() + " Success Notify Online");
+      if (error !== null) {
+        console.log(dater() + 'exec error: ' + error);
+      }
+    });
+  }else{
+    console.log(dater() + ' - ' + message2log);  
+  }
 };
 
 function connect(stream){
@@ -39,8 +51,9 @@ function connect(stream){
 };
 
 stream.on('connect', function() {
-    logger('Conectado a PushBullet');
-    notify(pusher,device,'IOT is UP');
+    //logger('Conectado a PushBullet');
+    notify(pusher,device,'IoT is UP');
+    logger('IoT is UP',true);
 });
 
 stream.on('close', function() {
@@ -53,15 +66,33 @@ stream.on('error', function(error) {
 });
 
 stream.on('push', function(push) {
+  console.log(push);
    if(push.title == 'command'){
     if (push.body == 'apagar') {
-      requester(request,url + 'on');
+//      requester(request,url + 'on');
       logger('Se apago el Foco');
     }
     if (push.body == 'encender') {
-      requester(request,url + 'off');
+//      requester(request,url + 'off');
       logger('Se encendio el Foco'); 
     }}
+});
+
+stream.on('message', function(message) {
+    // message received
+    logger('message received');
+    console.log(message); 
+});
+
+stream.on('nop', function() {
+    // nop message received
+    logger('nop message received'); 
+});
+
+stream.on('tickle', function(type) {
+    // tickle message received 
+    logger('tickle message received');
+    console.log(type);
 });
 
 connect(stream);
